4f3cfc034915164d3da29441672c9fc2b19f3006
Kirk-GD <69407553+Kirk-GD@users.noreply.github.com>
2020-08-08 21:36:40 -0400

    Update HelpEarthRot.cs

--- Scripts/HelpEarthRot.cs
+++ Scripts/HelpEarthRot.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class HelpEarthRot : MonoBehaviour
 {
     float speed = 13f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
